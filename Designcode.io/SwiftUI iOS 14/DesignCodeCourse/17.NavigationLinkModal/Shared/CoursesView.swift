84b1fa372f7f76fa40396c2df591bffa0acba3ff
巴糖 <380411544@qq.com>
2021-03-05 10:24:39 +0800

    Add
    16.SizeClassTabBar
    17.NavigationLinkModal
    18.AnimatingAboveNavigation
    19.LibraryContentModifiers
    20.CreateWidget

---
+++ Designcode.io/SwiftUI iOS 14/DesignCodeCourse/17.NavigationLinkModal/Shared/CoursesView.swift
@@ -0,0 +1,118 @@
+//
+//  CoursesView.swift
+//  DesignCodeCourse
+//
+//  Created by Meng To on 2020-07-15.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    @State var show = false
+    @Namespace var namespace
+    @Namespace var namespace2
+    @State var selectedItem: Course? = nil
+    @State var isDisabled = false
+    
+    var body: some View {
+        ZStack {
+            #if os(iOS)
+            content
+                .navigationBarHidden(true)
+            fullContent
+                .background(VisualEffectBlur(blurStyle: .systemMaterial).edgesIgnoringSafeArea(.all))
+            #else
+            content
+            fullContent
+                .background(VisualEffectBlur().edgesIgnoringSafeArea(.all))
+            #endif
+        }
+        .navigationTitle("Courses")
+    }
+    
+    var content: some View {
+        ScrollView {
+            VStack(spacing: 0) {
+                Text("Courses")
+                    .font(.largeTitle)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 16)
+                    .padding(.top, 54)
+                
+                LazyVGrid(
+                    columns: [GridItem(.adaptive(minimum: 160), spacing: 16)],
+                    spacing: 16
+                ) {
+                    ForEach(courses) { item in
+                        VStack {
+                            CourseItem(course: item)
+                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                                .frame(height: 200)
+                                .onTapGesture {
+                                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0)) {
+                                        show.toggle()
+                                        selectedItem = item
+                                        isDisabled = true
+                                    }
+                                }
+                                .disabled(isDisabled)
+                        }
+                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
+                    }
+                }
+                .padding(16)
+                .frame(maxWidth: .infinity)
+                
+                Text("Latest sections")
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 240))]) {
+                    ForEach(courseSections) { item in
+                        #if os(iOS)
+                        NavigationLink(destination: CourseDetail(namespace: namespace2)) {
+                            CourseRow(item: item)
+                        }
+                        #else
+                        CourseRow(item: item)
+                        #endif
+                    }
+                }
+                .padding()
+            }
+        }
+        .zIndex(1)
+    }
+    
+    @ViewBuilder
+    var fullContent: some View {
+        if selectedItem != nil {
+            ZStack(alignment: .topTrailing) {
+                CourseDetail(course: selectedItem!, namespace: namespace)
+                
+                CloseButton()
+                    .padding(16)
+                    .onTapGesture {
+                        withAnimation(.spring()) {
+                            show.toggle()
+                            selectedItem = nil
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                isDisabled = false
+                            }
+                        }
+                    }
+            }
+            .zIndex(2)
+            .frame(maxWidth: 712)
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
