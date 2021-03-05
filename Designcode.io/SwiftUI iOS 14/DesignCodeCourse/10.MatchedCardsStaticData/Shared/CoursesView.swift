c93b9f6edb3e639b91535cd9583be347ad5926e4
巴糖 <380411544@qq.com>
2021-03-05 10:23:59 +0800

    add
    10.MatchedCardsStaticData
    11.SelectedStateDisabled
    12.LazyGridLayout
    13.MatchedContainerzIndex
    14.PassingNamespace
    15.VisualEffectBlur

---
+++ Designcode.io/SwiftUI iOS 14/DesignCodeCourse/10.MatchedCardsStaticData/Shared/CoursesView.swift
@@ -0,0 +1,64 @@
+//
+//  CoursesView.swift
+//  DesignCodeCourse
+//
+//  Created by Meng To on 2020-07-15.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    @State var show = false
+    @Namespace var namespace
+    
+    var body: some View {
+        ZStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    ForEach(courses) { item in
+                        CourseItem(course: item)
+                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                            .frame(width: 335, height: 250)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+            }
+            if show {
+                ScrollView {
+                    CourseItem(course: courses[0])
+                        .matchedGeometryEffect(id: courses[0].id, in: namespace)
+                        .frame(height: 300)
+                    VStack {
+                        ForEach(0 ..< 20) { item in
+                            CourseRow()
+                        }
+                    }
+                    .padding()
+                }
+                .background(Color("Background 1"))
+                .transition(
+                    .asymmetric(
+                        insertion: AnyTransition
+                                    .opacity
+                                    .animation(Animation.spring().delay(0.3)),
+                        removal: AnyTransition
+                            .opacity
+                            .animation(.spring())
+                    )
+                )
+                .edgesIgnoringSafeArea(.all)
+            }
+        }
+        .onTapGesture {
+            withAnimation(.spring()) {
+                show.toggle()
+            }
+        }
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
